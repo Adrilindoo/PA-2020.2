17e76a92a101e78dd73cd2199b00a335b4b6b5ec
Adriel <adryeltc2001@gmail.com>
2021-03-15 23:28:32 -0300

    vai omi

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Salve salve galera to testando o bagulho aqui\n";
+    cout << "Salve salve galera to testando o bagulho aqui de novo aaaaaaaa\n";
     return 0;
 }
