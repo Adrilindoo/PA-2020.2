706968a2dfe228729aaf30ec3b47e476c39de3fd
Adriel <adryeltc2001@gmail.com>
2021-03-18 14:17:30 -0300

    misericordia tomara q n destrua

--- C++/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Salve salve galera to testando o bagulho aqui de novo aaaaaaaa\n";
-    return 0;
-}
--- Lista 1/Questão 3/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(void){
-    int i = 5, *p;
-    p = &i;
-    printf("\n%x %d %d %d %d\n", p, *p+2, **&p, 3**p, **&p+4);
-    return 0;
-}
